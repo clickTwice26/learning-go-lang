06582924bb2825bcbf1ecc1374da312b15298cf3
Shagato <47025267+clickTwice26@users.noreply.github.com>
2026-09-13 13:44:25 +0600

    completed constants

--- 4_constants/constants.go
+++ 4_constants/constants.go
@@ -4,13 +4,21 @@ import "fmt"
 
 func main(){
 
-	fmt.Println("Learning constants")
+	
 
+	fmt.Println("Learning constants")
+	// constant grouping 
+	const (
+		port = 5000
+		host = "localhost"
+	)
 	const name string = "Go-Lang"
-	name = "javascript"
+	// name = "javascript"
+	fmt.Println(port, host, name)
 
 	
 }
 
 // constant can be declared outside of the functiona also
 // := can't use this shorthand assignment outside of the function
+// constant value can't be changed and must be initialized with a value when declared
