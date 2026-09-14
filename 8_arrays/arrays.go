c52484ee03efa264e05a56c308d584a70f47ad74
Shagato <47025267+clickTwice26@users.noreply.github.com>
2026-09-15 01:50:26 +0600

    came back and completed the array

--- 8_arrays/arrays.go
+++ 8_arrays/arrays.go
@@ -18,6 +18,17 @@ func main(){
 	var strs[4]string
 	fmt.Println(strs)
 
-
+	
 	// remaining class : https://youtu.be/ZQmZmoziWj0?list=PLXQpH_kZIxTWUe-Ee-DZEX5gfeoo4tHV6&t=486
+	numbers := [3]int{1, 2, 3}
+	fmt.Println(numbers)
+
+	//2-D Class
+	dimen := [2][2]int{{3,4}, {5, 6}}
+	fmt.Println(dimen)
+
+
+	// When to use array
+	// 1. When we know the size and it is fixed , memory optmized
+	// 2. Constant time access O(1)
 }
