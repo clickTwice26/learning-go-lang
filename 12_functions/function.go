4a7b8e8e2d0698aa933c03cedc408ba740fbb348
Shagato <47025267+clickTwice26@users.noreply.github.com>
2026-09-20 00:31:16 +0600

    Add higher-order functions and Go tour basics

--- 12_functions/function.go
+++ 12_functions/function.go
@@ -9,11 +9,24 @@ func getLanguages() (string, string, string) {
 	return "go", "javascript", "c"
 }
 
+func processIt(fn func(a int) int){
+	fn(1)
+}
+func returnIt() func(a int) int{
+	return func(a int) int{
+		return 1	
+	}
+}
 func main(){
 	lang1, lang2, lang3 := getLanguages()
 	fmt.Println(add(1, 2))
 	fmt.Println(getLanguages())
 	fmt.Println(lang1, lang2, lang3)
 
-	// remaining :https://youtu.be/tG5qvZIUJx4?list=PLXQpH_kZIxTWUe-Ee-DZEX5gfeoo4tHV6&t=481
+	fn := func(a int) int {
+		return 2
+	}
+	processIt(fn)
+
+	
 }
